bfa8e5bfcecdee5bc34b8acab4a89589948e2e05
cuongnguyen-oceansmart <cuongnguyen3900@gmail.com>
2023-05-08 17:33:50 +0700

    adding Confession tab

---
+++ src/main/java/com/example/schoolmanagementsystem/controller/ConfessionController.java
@@ -0,0 +1,16 @@
+package com.example.schoolmanagementsystem.controller;
+
+import com.example.schoolmanagementsystem.service.ConfessionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ConfessionController {
+    private ConfessionService confessionService;
+
+    @Autowired
+    public ConfessionController (ConfessionService confessionService) {
+        super();
+        this.confessionService = confessionService;
+    }
+}
---
+++ src/main/java/com/example/schoolmanagementsystem/service/impl/ConfessionServiceImpl.java
@@ -0,0 +1,15 @@
+package com.example.schoolmanagementsystem.service.impl;
+
+import com.example.schoolmanagementsystem.repository.ConfessionRepository;
+import com.example.schoolmanagementsystem.service.ConfessionService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ConfessionServiceImpl implements ConfessionService {
+    private ConfessionRepository confessionRepository;
+
+    public ConfessionServiceImpl (ConfessionRepository confessionRepository) {
+        super();
+        this.confessionRepository = confessionRepository;
+    }
+}
